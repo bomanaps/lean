42ee6ec413ff70c18b74be3766f7b11f592745ac
bomanaps <mercy.boma35@yahoo.com>
2026-04-24 09:38:33 +0100

    address feedback on aggregator API handlers and tests

--- lean_client/http_api/src/lib.rs
+++ lean_client/http_api/src/lib.rs
@@ -1,11 +1,12 @@
-pub mod aggregator_controller;
+mod aggregator_controller;
 mod aggregator_handlers;
 mod config;
-pub mod handlers;
-pub mod routing;
+mod handlers;
+mod routing;
 mod server;
 
 pub use aggregator_controller::AggregatorController;
 pub use config::HttpServerConfig;
 pub use handlers::SharedStore;
+pub use routing::normal_routes;
 pub use server::run_server;
